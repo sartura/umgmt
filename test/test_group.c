6fd7fc75c737958087470e6694e515a5a65e27ce
Mateo Cindric <mateocindricdev@gmail.com>
2022-12-21 15:13:41 +0000

    Unit testing: wrap malloc for group data structure allocation

--- test/test_group.c
+++ test/test_group.c
@@ -1,3 +1,4 @@
+#include "umgmt/group.h"
 #include "umgmt/types.h"
 #include <stdarg.h>
 #include <stddef.h>
@@ -6,6 +7,10 @@
 #include <stdlib.h>
 
 #include <umgmt.h>
+
+#include "umgmt/group.c"
+
+#define UM_GROUP_T_SIZE sizeof(um_group_t)
 
 void *__wrap_malloc(size_t size);
 void *__real_malloc(size_t size);
@@ -30,11 +35,19 @@ static void test_group_new_correct(void **state)
 
     assert_null(group);
 
-    expect_value(__wrap_malloc, size, 48);
-    will_return(__wrap_malloc, __real_malloc(48));
+    expect_value(__wrap_malloc, size, UM_GROUP_T_SIZE);
+    will_return(__wrap_malloc, __real_malloc(UM_GROUP_T_SIZE));
 
     group = um_group_new();
     assert_non_null(group);
+
+    // assert all fields are set to NULL
+    assert_null(um_group_get_name(group));
+    assert_null(um_group_get_password(group));
+    assert_null(um_group_get_password_hash(group));
+    assert_null(um_group_get_members_head(group));
+    assert_null(um_group_get_admin_head(group));
+    assert_int_equal(um_group_get_gid(group), 0);
 
     um_group_free(group);
 }
@@ -47,7 +60,7 @@ static void test_group_new_incorrect(void **state)
 
     assert_null(group);
 
-    expect_value(__wrap_malloc, size, 48);
+    expect_value(__wrap_malloc, size, UM_GROUP_T_SIZE);
     will_return(__wrap_malloc, NULL);
 
     group = um_group_new();
