750d8bff9f2216e20a0bb2c8d88c2451e18099c4
Mateo Cindric <mateocindricdev@gmail.com>
2022-12-21 14:30:27 +0000

    Unit testing: add password hash setting tests

--- test/test_user.c
+++ test/test_user.c
@@ -32,6 +32,9 @@ static void test_user_set_home_path_incorrect(void **state);
 static void test_user_set_shell_path_correct(void **state);
 static void test_user_set_shell_path_incorrect(void **state);
 
+static void test_user_set_password_hash_correct(void **state);
+static void test_user_set_password_hash_incorrect(void **state);
+
 int main(void)
 {
     const struct CMUnitTest tests[] = {
@@ -47,6 +50,8 @@ int main(void)
         cmocka_unit_test(test_user_set_home_path_incorrect),
         cmocka_unit_test(test_user_set_shell_path_correct),
         cmocka_unit_test(test_user_set_shell_path_incorrect),
+        cmocka_unit_test(test_user_set_password_hash_correct),
+        cmocka_unit_test(test_user_set_password_hash_incorrect),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
@@ -63,13 +68,15 @@ static void test_user_new_correct(void **state)
     assert_non_null(user);
 
     // assert all fields are set to NULL
+
+    // char* user data
     assert_null(um_user_get_name(user));
     assert_null(um_user_get_password(user));
     assert_null(um_user_get_gecos(user));
     assert_null(um_user_get_home_path(user));
     assert_null(um_user_get_shell_path(user));
 
-    // integer data
+    // integer user data
     assert_int_equal(um_user_get_uid(user), 0);
     assert_int_equal(um_user_get_gid(user), 0);
 
@@ -237,5 +244,37 @@ static void test_user_set_shell_path_incorrect(void **state)
     will_return(__wrap_strdup, NULL);
 
     error = um_user_set_shell_path(user, shell_path);
+    assert_int_equal(error, -1);
+}
+
+static void test_user_set_password_hash_correct(void **state)
+{
+    (void)state;
+
+    int error = 0;
+    char *password_hash = "0255a4237b05193cfbd43edb3d11c6296f7ea6825315e83d168f7b815674d2027a5bfed3582554db15982545a24b"
+                          "09cb7b8323cf595c947e927ba610497f3537";
+    um_user_t *user = um_user_new();
+
+    expect_string(__wrap_strdup, s, password_hash);
+    will_return(__wrap_strdup, password_hash);
+
+    error = um_user_set_password_hash(user, password_hash);
+    assert_int_equal(error, 0);
+}
+
+static void test_user_set_password_hash_incorrect(void **state)
+{
+    (void)state;
+
+    int error = 0;
+    char *password_hash = "0255a4237b05193cfbd43edb3d11c6296f7ea6825315e83d168f7b815674d2027a5bfed3582554db15982545a24b"
+                          "09cb7b8323cf595c947e927ba610497f3537";
+    um_user_t *user = um_user_new();
+
+    expect_string(__wrap_strdup, s, password_hash);
+    will_return(__wrap_strdup, NULL);
+
+    error = um_user_set_password_hash(user, password_hash);
     assert_int_equal(error, -1);
 }
